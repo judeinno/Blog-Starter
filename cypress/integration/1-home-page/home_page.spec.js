4a6ba840cd047733aff2ea7539bfa4051356a370
judeinno <judeinno@gmail.com>
2022-03-23 10:08:15 +0300

    Start testing home page

--- cypress/integration/1-home-page/home_page.spec.js
+++ cypress/integration/1-home-page/home_page.spec.js
@@ -1,15 +1,39 @@
 /// <reference types="cypress" />
 
-describe('Test home page', () => {
-  beforeEach(() => {
+context('Test home page', () => {
+  beforeEach(function () {
     cy.visit('')
-  })
-
-  it('Blog desc text', () => {
-    cy.get('.container').eq(1).within(() => {
-      cy.get('h4').should('exist')
-      cy.get('h4').first().should('have.text', 'A statically generated blog example using Next.js and Markdown.')
+    cy.fixture('home_page.json').then((data) => {
+      this.siteData = data
     })
   })
 
+  it('Test title', function () {
+    cy.getElementByIndex('.container', 1).within(() => {
+      cy.get('h1').first()
+        .should('have.text', this.siteData.siteHeading)
+      cy.get('h4').should('exist')
+      cy.get('h4').first()
+        .should('have.text', this.siteData.siteDesc)
+      cy.get('h4').first().then(() => {
+        cy.get('a').should('have.attr', 'href')
+        .and('include', 'https://nextjs.org/')
+      })
+    })
+  })
+
+  it('Verify that all images have a src', () => {
+    cy.get('img').should('have.attr', 'src').and('contain.value')
+  })
+
+  it('Verify that all paragraphs have text', () => {
+    cy.get('p').should('contain.text')
+  })
+
+  it('Verify that all sub titles contain text have text', () => {
+    cy.get('.text-lg').should('contain.text')
+  })
+  
 })
+
+// cy.url().should('include', 'posts/dynamic-routing')
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,7 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('getElementByIndex', (property, index) => {
+    cy.get(property).eq(index)
+})
